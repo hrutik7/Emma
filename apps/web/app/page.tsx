941e98fdb8ca060e121e70776aecc5fcf764db31
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-11-29 21:08:03 +0530

    clerk auth added

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,20 +1,19 @@
 import Image from "next/image";
 import { Card } from "ui";
 import styles from "./page.module.css";
-// import { NavbarDefault } from "./components/Navbar";
-
+import { SignInButton } from "@clerk/nextjs";
 import "ui/styles.css";
 import { Menubar } from "./components/ui/menubar";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-// import { MenubarDemo } from "./components/Navbar";
-// 010001
+
 
 export default function Page(): JSX.Element {
   return (
    <div className="md:mr-[2%] md:ml-[2%]">
   <Navbar />
   <Hero />
+  
    </div>
   );
 }
