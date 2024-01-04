fb2b6fda96f54aa93a19c693338cc44076d2d5dc
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2024-01-04 15:55:23 +0530

    next auth set

--- packages/Backend/src/server/api/routers/users.ts
+++ packages/Backend/src/server/api/routers/users.ts
@@ -1,25 +1,25 @@
-import {z} from "zod";
+import { z } from "zod";
 import { TRPCError } from "@trpc/server";
-import {createTRPCRouter, publicProcedure} from "../../trpc";
-import {  Prisma, type PrismaClient } from "@prisma/client";
+import { createTRPCRouter, publicProcedure } from "../../trpc";
+import { Prisma, type PrismaClient } from "@prisma/client";
 
 const usersSchema = z.object({
-    id: z.string(),
-    email: z.string().email(),
-  });
-  
-  const updateUserSchema = z.object({
-    email: z.string().email(),
-  });
+  id: z.string(),
+  email: z.string().email(),
+});
 
-  const getUsers = async () => {
+const updateUserSchema = z.object({
+  email: z.string().email(),
+});
+
+const getUsers = async () => {
+  const users = await prisma?.users.findMany();
+  return users;
+};
+
+export const userRouter = createTRPCRouter({
+  users: publicProcedure.query(async () => {
     const users = await prisma?.users.findMany();
     return users;
-  }
-
-  export const userRouter = createTRPCRouter({
-     users : publicProcedure.query(async()=>{
-      const users = await prisma?.users.findMany();
-      return users;
-    })
-  })
+  }),
+});
--- packages/Backend/src/server/trpc.ts
+++ packages/Backend/src/server/trpc.ts
@@ -1,27 +1,49 @@
-import { initTRPC ,inferAsyncReturnType} from '@trpc/server';
- 
+import { initTRPC, inferAsyncReturnType } from "@trpc/server";
+import {
+  getAuth,
+  SignedInAuthObject,
+  SignedOutAuthObject,
+} from "@clerk/nextjs/server";
+import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
 import * as trpcExpress from "@trpc/server/adapters/express";
+import { prisma } from "./db";
+interface AuthContext {
+  auth: SignedInAuthObject | SignedOutAuthObject;
+}
 
-// created for each request
-export const createContext = async ({
-  req,
-  res,
-}: trpcExpress.CreateExpressContextOptions) => {
+export const createContextInner = async ({ auth }: AuthContext) => {
   return {
-    req,
-    res,
+    auth,
+    prisma,
   };
+};
+// created for each request
+export const createContext = async (opts: CreateNextContextOptions) => {
+  return createContextInner({ auth: getAuth(opts.req) });
 }; // context
 type Context = inferAsyncReturnType<typeof createContext>;
-export const t = initTRPC.context<Context>().create()
- 
+export const t = initTRPC.context<Context>().create();
 /**
  * Export reusable router and procedure helpers
  * that can be used throughout the router
  */
+
+const isAuthed = t.middleware(({ next, ctx }) => {
+  if (!ctx.auth.userId) {
+    throw new Error("Not authenticated");
+  }
+  return next({
+    ctx: {
+      auth: ctx.auth,
+    },
+  });
+});
 export const createTRPCRouter = t.router;
+
 export const publicProcedure = t.procedure;
+
+export const protectedProcedure = t.procedure.use(isAuthed);
